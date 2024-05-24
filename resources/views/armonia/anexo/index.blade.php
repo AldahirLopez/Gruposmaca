da8cf284c4f7d703bbec301bf0e99394a938ec47
Hector Aldahir Lap <hectoralr21@gmail.com>
2024-05-24 12:07:56 -0600

    menu servicios

--- resources/views/armonia/anexo/index.blade.php
+++ resources/views/armonia/anexo/index.blade.php
@@ -7,6 +7,7 @@
     </div>
     <div class="section-body">
         <div class="row">
+
             <div class="col-lg-4">
                 <div class="card">
                     <div class="card-body">
@@ -17,36 +18,6 @@
                                 <h2><span></span></h2>
                                 <p class="m-b-0 text-right"><a href="{{ route('servicio_anexo.index') }}">Ver más...</a>
                                 </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-lg-4">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title1" style="margin-top: 20px;">Pagos</h5>
-                        <div class="d-flex justify-content-between">
-                            <h2 class="text-right"><i class="bi bi-currency-dollar"></i></h2>
-                            <div class="d-flex flex-column justify-content-between align-items-center">
-                                <h2><span></span></h2>
-                                <p class="m-b-0 text-right"><a href="#">Ver más...</a></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-lg-4">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title1" style="margin-top: 20px;">Estaciones</h5>
-                        <div class="d-flex justify-content-between">
-                            <h2 class="text-right"><i class="bi bi-droplet-half"></i></h2>
-                            <div class="d-flex flex-column justify-content-between align-items-center">
-                                <h2><span></span></h2>
-                                <p class="m-b-0 text-right"><a href="#">Ver más...</a></p>
                             </div>
                         </div>
                     </div>
@@ -67,6 +38,7 @@
                     </div>
                 </div>
             </div>
+
         </div>
     </div>
 </section>
