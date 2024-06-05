9ddd99eb11aa04cbbb6aa8cc6255d0d92bf6d4c0
Hector-PC-Trabajo <hectoralr21@gmail.com>
2024-06-05 12:14:08 -0600

    Cambios menu

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,23 +1,55 @@
 @php
     use Illuminate\Support\Facades\Auth;
 @endphp
+
+<style>
+    .logo img {
+        max-height: 50px;
+        /* Ajusta el tamaño según sea necesario */
+        margin-right: 10px;
+        /* Espacio entre la imagen y el texto */
+    }
+
+    .logo div {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .logo span {
+        font-size: 14px;
+        white-space: nowrap;
+        /* Evita que el texto se divida en varias líneas */
+        overflow: hidden;
+        /* Oculta cualquier desbordamiento de texto */
+        text-overflow: ellipsis;
+        /* Agrega puntos suspensivos si el texto es demasiado largo */
+        max-width: 150px;
+        /* Ajusta el tamaño según sea necesario */
+    }
+</style>
+
+
 <header id="header" class="header fixed-top d-flex align-items-center">
 
     <div class="d-flex align-items-center justify-content-between">
-        <a href="#" class="logo d-flex align-items-center">
-            <img src="{{ asset('assets/img/logo.png') }}" alt="">
-            <span class="d-none d-lg-block">Grupo SMACA</span>
+        <a href="#" class="logo d-flex align-items-center flex-column flex-lg-row">
+            <img src="{{ asset('assets/img/logoarmonia.png') }}" alt="">
+            <div class="d-flex flex-column text-center text-lg-start">
+                <span class="d-none d-lg-block">Armonia y Contraste</span>
+                <span class="d-none d-lg-block">Ambiental S.A. DE C.V.</span>
+            </div>
         </a>
         <i class="bi bi-list toggle-sidebar-btn"></i>
     </div><!-- End Logo -->
+
 
     <nav class="header-nav ms-auto">
         <ul class="d-flex align-items-center">
             <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
             <script>
-                $(document).ready(function() {
+                $(document).ready(function () {
                     function fetchNotifications() {
-                        $.get('/fetch-notifications', function(data) {
+                        $.get('/fetch-notifications', function (data) {
                             $('#notifications-container').html(data);
                         });
                     }
@@ -27,9 +59,9 @@
                 });
             </script>
             @if(auth()->check() && auth()->user()->hasRole('Administrador'))
-            <div id="notifications-container" class="d-flex align-items-center">
-                @include('partials.notifications-servicios')
-            </div>
+                <div id="notifications-container" class="d-flex align-items-center">
+                    @include('partials.notifications-servicios')
+                </div>
             @endif
 
             <li class="nav-item dropdown pe-4">
@@ -37,17 +69,17 @@
                 <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
                     <!-- <img src="{{ asset('assets/img/profile-img.jpg') }}" alt="Profile" class="rounded-circle">-->
                     @auth
-                    <span class="d-none d-md-block dropdown-toggle ps-2">{{ Auth::user()->name }}</span>
+                        <span class="d-none d-md-block dropdown-toggle ps-2">{{ Auth::user()->name }}</span>
                     @endauth
                 </a><!-- End Profile Iamge Icon -->
 
                 <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
                     <li class="dropdown-header">
                         @auth
-                        <h6>{{ Auth::user()->name }}</h6>
-                        @foreach(Auth::user()->getRoleNames() as $role)
-                        <span>{{ $role }}</span>
-                        @endforeach
+                            <h6>{{ Auth::user()->name }}</h6>
+                            @foreach(Auth::user()->getRoleNames() as $role)
+                                <span>{{ $role }}</span>
+                            @endforeach
                         @endauth
                     </li>
                     <li>
@@ -55,7 +87,8 @@
                     </li>
 
                     <li>
-                        <a class="dropdown-item d-flex align-items-center" href="{{ route('usuarios.showchangepasswordform', ['id' => Auth::user()->id]) }}">
+                        <a class="dropdown-item d-flex align-items-center"
+                            href="{{ route('usuarios.showchangepasswordform', ['id' => Auth::user()->id]) }}">
                             <i class="bi bi-person"></i>
                             <span>Cambiar Contraseña</span>
                         </a>
