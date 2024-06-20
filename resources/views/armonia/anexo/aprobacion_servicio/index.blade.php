6ad7e4b6ee2e0097b7d2f01008ed9cb12ebcbb0e
Hector-PC-Trabajo <hectoralr21@gmail.com>
2024-06-20 13:37:41 -0600

    Cambios vista

--- resources/views/armonia/anexo/aprobacion_servicio/index.blade.php
+++ resources/views/armonia/anexo/aprobacion_servicio/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <section class="section">
     <div class="section-header"> 
-        <h3 class="page__heading">Aprobacion servicios anexo 30</h3>
+        <h3 class="page__heading">Aprobaciones Servicios Anexo 30</h3>
     </div>
     <div class="section-body">
         <div class="row">
